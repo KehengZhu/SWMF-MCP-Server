137f80c6eebcb54f20b8962d14560f3eee8d06fc
Keheng Zhu <zkeheng@umich.edu>
2026-04-23 16:32:52 -0400

    removed build

--- build/lib/swmf_mcp_server/reference/xml.py
+++
@@ -1,3 +0,0 @@
-from ..parsing.xml_parser import normalize_command_name, parse_param_xml_file
-
-__all__ = ["normalize_command_name", "parse_param_xml_file"]
--- build/lib/swmf_mcp_server/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = [
-    "get_context",
-    "get_evidence",
-    "get_workflow_guidance",
-    "inspect_artifact",
-    "compare_artifacts",
-]
